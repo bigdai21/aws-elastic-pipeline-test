91bbe18d97c3e72a210ace7413ec138f7b5a2f87
bigdai21 <tolochko228@gmail.com>
2022-07-28 20:36:58 +0300

    add health

--- config/database.ts
+++ config/database.ts
@@ -44,12 +44,12 @@ const databaseConfig: DatabaseConfig = {
       },
       migrations: {
         naturalSort: true,
+        disableRollbacksInProduction: true,
       },
-      healthCheck: false,
-      debug: false,
+      healthCheck: true,
+      debug: true,
     },
-
-  }
+  },
 }
 
 export default databaseConfig
